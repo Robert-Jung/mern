742c06f4a20fed0f201faee6487ac8d880502749
Robert-Jung <rjinjung@gmail.com>
2017-08-28 15:59:43 -0700

    GET and POST to db.

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ const mongoUrl = process.env.MONGOLAB_URI
 const port = process.env.API_PORT || 3001
 mongoose.connect(mongoUrl)
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Credentials', 'true')
@@ -27,6 +30,26 @@ app.use((req, res, next) => {
 router.get('/', (req, res) => {
   res.json({ message: 'API Initialized!'})
 })
+
+router.route('/comments')
+  .get((req, res) => {
+    Comment.find((err, comments) => {
+      if (err)
+        res.send(err)
+      res.json(comments)
+    })
+  })
+  .post((req, res) => {
+    let comment = new Comment()
+    comment.author = req.body.author
+    comment.text = req.body.text
+
+    comment.save((err) => {
+      if (err)
+        res.send(err)
+      res.json({ message: 'Comment successfully added!'})
+    })
+  })
 
 app.use('/api', router)
 
