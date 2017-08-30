048de0a4c2e1d8adf013a3d725357b86ef61533d
Robert-Jung <rjinjung@gmail.com>
2017-08-29 22:13:28 -0700

    Adding update route.

--- server.js
+++ server.js
@@ -51,6 +51,20 @@ router.route('/comments')
     })
   })
 
+router.route('/comments/:comment_id')
+  .put((req, res) => {
+    Comment.findById(req.params.comment_id, (err, comment) => {
+      if (err)
+        res.send(err)
+      (req.body.author) ? comment.author = req.body.author : null
+      (req.body.text) ? comment.text = req.body.text : null
+      comment.save((err) => {
+        if (err) res.send(err)
+        res.json({ message: 'Comment has been updated' })
+      })
+    })
+  })
+
 app.use('/api', router)
 
 app.listen(port, () => {
